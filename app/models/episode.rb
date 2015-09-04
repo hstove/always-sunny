a8a4a644b14da9db54210491fce0d2b82094a32f
hank <hstove@gmail.com>
2015-09-04 15:23:49 -0700

    cache homepage

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,8 @@
 class Episode < ActiveRecord::Base
   validates_presence_of :season, :number, :title#, :day_of_week, :hour_of_day, :time_of_hour
   validates_uniqueness_of :number, scope: :season
+
+  after_save do
+    Rails.cache.clear
+  end
 end
